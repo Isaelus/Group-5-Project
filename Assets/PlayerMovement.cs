5a8ffeb41b58a49c891945621eec944ac6ebfc47
Isaelus <78238276+Isaelus@users.noreply.github.com>
2022-09-29 23:19:31 -0700

    Updates to PlayerMovement.cs
    Made it so you can only jump while grounded.

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -4,37 +4,64 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    //movement variables
+    [SerializeField] private float movSpeed;
+    [SerializeField] private float jumpForce;
+    [SerializeField] private float dashForce;
 
-    private Rigidbody2D player1;
-    
+    //detection variables
+    [SerializeField] private LayerMask jumpableGround;
+    [SerializeField] private Rigidbody2D player1;
+    [SerializeField] private BoxCollider2D p1Hitbox;
 
+    /*
+    //cooldown variables
+    public float dashCooldownTime = 2f; //working out how to dash right now - Brylle
+    [SerializeField] private float currentDashCD = 0f;
+    */
 
     // Start is called before the first frame update
-    private void Start()
+    void Start()
     {
         Debug.Log("New Game Start - view from console");
         player1 = GetComponent<Rigidbody2D>();
+        p1Hitbox = GetComponent<BoxCollider2D>();
 
+        movSpeed = 4f;
+        jumpForce = 4f;
+        dashForce = 6f;
     }
 
     // Update is called once per frame
-    private void Update()
+    void Update()
     {
-        
+
+        //horizontal movement
         float dirX  = Input.GetAxisRaw("Horizontal");
-        player1.velocity = new Vector2(dirX * 7f, player1.velocity.y);
+        player1.velocity = new Vector2(dirX * movSpeed, player1.velocity.y);
 
-
-        if (Input.GetButtonDown("Jump"))
+        // Jump
+        if (Input.GetButtonDown("Jump") && IsGrounded())
         {
-            player1.velocity = new Vector2(player1.velocity.x, 14f);
+            player1.velocity = new Vector2(player1.velocity.x, jumpForce);
         }
 
-       
-
-        
-
+        /*Dash
+        if (Time.time > currentDashCD) {
+            if (Input.GetButtonDown("Dash")) {
+                
+                print("dashed!");
+                currentDashCD = Time.time + dashCooldownTime;
+            }
+        }
+        */
+    }
+    //this method checks if the player is grounded.
+    private bool IsGrounded()
+    {
+        return Physics2D.BoxCast(p1Hitbox.bounds.center, p1Hitbox.bounds.size, 0f, Vector2.down, 0.1f, jumpableGround);
     }
 
     
+
 }
